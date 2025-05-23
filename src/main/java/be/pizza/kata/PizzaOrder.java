78862766ce2dbaf016dcc521422e36687f9a9ea0
moermansj <jonathan.moermans2@gmail.com>
2025-05-23 22:35:26 +0200

    delombok

--- src/main/java/be/pizza/kata/PizzaOrder.java
+++ src/main/java/be/pizza/kata/PizzaOrder.java
@@ -5,11 +5,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.Data;
 
+import java.util.Objects;
 import java.util.UUID;
 
-@Data
 @Entity
 public class PizzaOrder {
 
@@ -22,4 +21,52 @@ public class PizzaOrder {
     private String pizza;
     @Column(name = "size", nullable = false)
     private String size;
+
+    protected PizzaOrder() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(String pizza) {
+        this.pizza = pizza;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "PizzaOrder{" +
+                "id=" + id +
+                ", pizza='" + pizza + '\'' +
+                ", size='" + size + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof PizzaOrder that)) return false;
+        return Objects.equals(id, that.id) && Objects.equals(pizza, that.pizza) && Objects.equals(size, that.size);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, pizza, size);
+    }
+
 }
